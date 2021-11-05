818277d0178fe23ae3a8c94901002150eb61fc65
Jared Stevens <jared.stevens1229@gmail.com>
2021-11-05 17:44:50 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+// Canvas variables
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 
+// Ball variable
 var ballRadius = 10;
 
+// Paddle variables
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width-paddleWidth) / 2;
 
+// Moving paddle variables
 var rightPressed = false;
 var leftPressed = false;
+
+// Brick field variables
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+// Bricks array
+var bricks = [];
+for(var c=0; c<brickColumnCount; c++) {
+  bricks[c] = [];
+  for(var r=0; r<brickRowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0 };
+  }
+}
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -36,6 +58,7 @@ if(e.key == "Right" || e.key == "ArrowRight") {
   }
 }
 
+// Ball function
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -44,6 +67,7 @@ function drawBall() {
   ctx.closePath();
 }
 
+// Paddle function
 function drawPaddle() {
   ctx.beginPath();
   ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
@@ -52,10 +76,29 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+// Bricks function
+function drawBricks() {
+  for(var c=0; c<brickColumnCount; c++) {
+    for(var r=0; r<brickRowCount; r++) {
+      var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+      var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath():
+    }
+  }
+}
+
+// Game mechanics function
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   drawPaddle();
+  drawBricks();
   
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
     dx = -dx;
